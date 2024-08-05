a24f43d26882e1d568ed23eecb4e4e5fa6227828
Durmuş Demirtaş <demirtasdurmus@gmail.com>
2024-08-05 08:38:11 +0300

    chore: test rate limiting-2

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,13 +5,13 @@ import {
   NextFetchEvent,
   NextMiddleware,
 } from "next/server";
-import { Ratelimit } from "@upstash/ratelimit";
-import { kv } from "@vercel/kv";
+// import { Ratelimit } from "@upstash/ratelimit";
+// import { kv } from "@vercel/kv";
 
-const ratelimit = new Ratelimit({
-  redis: kv,
-  limiter: Ratelimit.slidingWindow(5, "10 s"),
-});
+// const ratelimit = new Ratelimit({
+//   redis: kv,
+//   limiter: Ratelimit.slidingWindow(5, "10 s"),
+// });
 
 // Trigger this middleware to run on the `/secret-page` route
 export const config: MiddlewareConfig = {
@@ -19,19 +19,20 @@ export const config: MiddlewareConfig = {
 };
 
 export const middleware: NextMiddleware = async (req, event) => {
-  const ip = req.headers.get("CF-Connecting-IP") || "127.0.0.1";
+  // const ip = req.headers.get("CF-Connecting-IP") || "127.0.0.1";
 
-  console.log("Ip ------->: ", ip, "--------------");
+  // console.log("Ip ------->: ", ip, "--------------");
 
-  const { success, remaining } = await ratelimit.limit(ip);
+  // const { success, remaining } = await ratelimit.limit(ip);
 
-  console.log("Remaining---------> ", remaining);
+  // console.log("Remaining---------> ", remaining);
 
-  const blockedUrl = `/blocked?ip=${ip}`;
+  // const blockedUrl = `/blocked?ip=${ip}`;
 
-  return success
-    ? NextResponse.next()
-    : NextResponse.redirect(new URL(blockedUrl, req.url));
+  // return success
+  //   ? NextResponse.next()
+  //   : NextResponse.redirect(new URL(blockedUrl, req.url));
+  return NextResponse.next();
 };
 
 // // Extract country. Default to US if not found.
