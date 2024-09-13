33ea48be57df0b91a655aecab8399237c8f1da28
Durmuş Demirtaş <demirtasdurmus@gmail.com>
2024-09-13 12:20:19 +0300

    refactor: refactor contact and footer components

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -17,14 +17,15 @@ export default function Contact() {
             to discuss a project.
           </p>
           <ul className="mt-4 space-y-2 animate-fade-in-up">
-            <li className="flex items-center">
-              <MailIcon className="mr-2 h-5 w-5 text-text-neutral-400" />
-              <span>demirtasdurmus@gmail.com</span>
+            <li>
+              <a
+                href="mailto:demirtasdurmus@gmail.com"
+                className="flex items-center text-neutral-300 hover:text-neutral-400 transition-colors duration-200"
+              >
+                <MailIcon className="mr-2 h-5 w-5" />
+                <span>demirtasdurmus@gmail.com</span>
+              </a>
             </li>
-            {/* <li className="flex items-center">
-              <PhoneIcon className="mr-2 h-5 w-5 text-text-neutral-400" />
-              <span>+9 (555) </span>
-            </li> */}
           </ul>
         </div>
       </div>
@@ -32,10 +33,11 @@ export default function Contact() {
   );
 }
 
-function MailIcon(props: any) {
+interface MailIconProps extends React.SVGProps<SVGSVGElement> {}
+
+const MailIcon: React.FC<MailIconProps> = (props) => {
   return (
     <svg
-      {...props}
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
@@ -45,28 +47,10 @@ function MailIcon(props: any) {
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
+      {...props}
     >
       <rect width="20" height="16" x="2" y="4" rx="2" />
       <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
     </svg>
   );
-}
-
-function PhoneIcon(props: any) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
-    </svg>
-  );
-}
+};
